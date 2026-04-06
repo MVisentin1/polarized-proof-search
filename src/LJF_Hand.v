2ff44986e4f3775f40229f01ab239ea67d20ae68
melvyn <melvyn2@dnsense.pub>
2026-04-06 15:35:26 -0400

    Re-add by-hand proofs for comparison

---
+++ src/LJF_Hand.v
@@ -0,0 +1,92 @@
+From Stdlib Require Import List.
+
+From CARVe Require Import contexts.list algebras.dill.
+From VST.msl Require Import sepalg.
+
+From LJF Require Import LJF_Logic LJF_Prover.
+
+Lemma True_proveable: (ufc nil True Unbracketed).
+Proof.
+  apply ufc_R_box.
+  T_bracketable.
+  apply ufc_R_f.
+  T_exh.
+  T_positive.
+  apply rfc_R_True.
+  T_exh.
+Qed.
+
+Lemma Impl_trans_forward_chaining : forall (x y z : nat),
+  let a := Atom Pos x in
+  let b := Atom Pos y in
+  let c := Atom Neg z in
+  let C := (a, omega) :: (Impl a b, omega) :: (Impl b c, omega) :: nil in
+  lfc C (Impl a b) c.
+Proof.
+  intros.
+  apply lfc_L_Impl.
+    - T_exh.
+    - apply rfc_I_r.
+      + T_exh.
+      + T_has_entry.
+      + T_positive.
+      + constructor.
+    - apply lfc_R_l.
+      + T_exh.
+      + T_positive.
+      + eapply ufc_L_box.
+        -- eexists. constructor.
+        -- T_permeable.
+        -- eapply ufc_L_f.
+          ++ T_exh.
+          ++ simpl. right. right. right. left. reflexivity.
+          ++ T_negative.
+          ++ apply lfc_L_Impl.
+            --- T_exh.
+            --- apply rfc_I_r.
+              +++ T_exh.
+              +++ T_has_entry.
+              +++ T_positive.
+              +++ constructor.
+            --- apply lfc_I_l.
+              +++ T_exh.
+              +++ T_negative.
+              +++ constructor.
+Qed.
+
+
+Lemma Impl_trans_backward_chaining_example : forall (x y z : nat),
+  let a := Atom Pos x in
+  let b := Atom Neg y in
+  let c := Atom Neg z in
+  let C := (a, omega) :: (Impl a b, omega) :: (Impl b c, omega) :: nil in
+  lfc C (Impl b c) c.
+Proof.
+  intros x y z a b c C.
+  apply lfc_L_Impl.
+    - T_exh.
+    - apply rfc_R_r.
+      + T_exh.
+      + constructor.
+      + apply ufc_R_box.
+        -- T_bracketable.
+        -- eapply ufc_L_f.
+          ++ T_exh.
+          ++ simpl. right. left. reflexivity.
+          ++ constructor.
+          ++ apply lfc_L_Impl.
+            --- T_exh.
+            --- apply rfc_I_r.
+              +++ T_exh.
+              +++ T_has_entry.
+              +++ constructor.
+              +++ constructor.
+            --- apply lfc_I_l.
+              +++ T_exh.
+              +++ constructor.
+              +++ constructor.
+    - apply lfc_I_l.
+      + T_exh.
+      + constructor.
+      + constructor.
+Qed.
