7d292c348d1c832643bb26c5cda87b47d779d10d
melvyn <melvyn2@dnsense.pub>
2026-04-15 19:36:32 +0000

    remove unnecessary `solve` tactics

--- src/LJF4_Prover.v
+++ src/LJF4_Prover.v
@@ -6,43 +6,43 @@ From VST.msl Require Import sepalg.
 
 From LJF Require Import LJF4_Rules LJF_SharedLogic.
 
-Ltac T_exh := solve [
-  lazymatch goal with
+Ltac T_exh := lazymatch goal with
   | [|- exh _ ] => simpl; repeat split; try (apply halo); try (apply halz); try (apply I)
-  end] || fail "T_exh : context is not exhausted, or goal is not an exh lookup predicate"
+  end
+  || fail "T_exh : context is not exhausted, or goal is not an exh lookup predicate"
 .
 
 (*Use when we know what we are looking  for, not for making decisions*)
-Ltac T_has_entry := solve [
-  lazymatch goal with
+Ltac T_has_entry := lazymatch goal with
   | [|- has_entry _ _] => simpl; repeat ((left; reflexivity) || right)
-  end] || fail "T_has_entry : context does not contain entry, or goal is not an entry lookup predicate"
+  end
+  || fail "T_has_entry : context does not contain entry, or goal is not an entry lookup predicate"
 .
 
-Ltac T_upd_rel_ex := solve [
+Ltac T_upd_rel_ex :=
   lazymatch goal with
   | [|- upd_rel_ex _ _ _ _] => eexists; constructor
-  end] || fail "T_upd_rel_ex : context could not be updated, or goal is not a context update predicate"
+  end
+  || fail "T_upd_rel_ex : context could not be updated, or goal is not a context update predicate"
 .
 
 
-Ltac T_noloop con formula path :=
-lazymatch path with
-| nil => idtac
-| (?con', ?formula') :: ?rest =>
-    lazymatch con' with
-    | con =>
-        lazymatch formula' with
-        | formula =>
-            fail "T_noloop: attempted to left focus on repeated formula and context"
-        | _ => T_noloop con formula rest
-        end
-    | _ => T_noloop con formula rest
-    end
-end.
+Ltac T_noloop con formula path := lazymatch path with
+  | nil => idtac
+  | (?con', ?formula') :: ?rest =>
+      lazymatch con' with
+      | con =>
+          lazymatch formula' with
+          | formula =>
+              fail "T_noloop: attempted to left focus on repeated formula and context"
+          | _ => T_noloop con formula rest
+          end
+      | _ => T_noloop con formula rest
+      end
+  end
+.
 
-Ltac T_positive := solve
-  [lazymatch goal with
+Ltac T_positive := lazymatch goal with
   | [|- positive ?a] => let a' := (eval hnf in a) in
     lazymatch a' with
     | Atom Pos _ => apply Pos_atom
@@ -51,7 +51,8 @@ Ltac T_positive := solve
     | AndP _ _ => apply Pos_and
     | Or _ _ => apply Pos_or
     end
-  end] || fail "T_positive: formula is not positive, or goal is not a positivity predicate"
+  end
+  || fail "T_positive: formula is not positive, or goal is not a positivity predicate"
 .
 
 Ltac T_is_positive k :=
@@ -65,15 +66,15 @@ Ltac T_is_positive k :=
   end
 .
 
-Ltac T_negative := solve
-  [lazymatch goal with
+Ltac T_negative := lazymatch goal with
   | [|- negative ?a] => let a' := (eval hnf in a) in
     lazymatch a' with
     | Atom Neg _ => apply Neg_atom
     | AndN _ _ => apply Neg_and
     | Impl _ _ => apply Neg_imp
     end
-  end] || fail "T_negative: formula is not negative, or goal is not a negativity predicate,"
+  end
+  || fail "T_negative: formula is not negative, or goal is not a negativity predicate,"
 .
 
 Ltac T_is_negative k:=
@@ -85,23 +86,24 @@ Ltac T_is_negative k:=
   end
 .
 
-Ltac T_permeable := solve
-  [lazymatch goal with
+Ltac T_permeable := lazymatch goal with
   | [|- permeable ?a ] => let a' := (eval hnf in a) in
     lazymatch a' with
     | Atom Pos _ => apply Permeable_pos_atom; [> apply Is_atom | apply Pos_atom]
     | _ => apply Permeable_neg; T_negative
     end
-  end] || fail "T_permeable: formula is not permeable, or goal is either not a permeability predicate".
+  end
+  || fail "T_permeable: formula is not permeable, or goal is either not a permeability predicate"
+.
 
-Ltac T_bracketable := solve
-  [lazymatch goal with
+Ltac T_bracketable := lazymatch goal with
   | [|- bracketable ?a ] => let a' := (eval hnf in a) in
     lazymatch a' with
     | Atom Neg _ => apply Bracketable_neg_atom ; [> apply Is_atom | apply Neg_atom ]
     | _ => apply Bracketable_pos ; T_positive
     end
-  end] || fail "T_bracketable: formula is not bracketable, or goal is either not a bracketability predicate"
+  end
+  || fail "T_bracketable: formula is not bracketable, or goal is either not a bracketability predicate"
 .
 
 (*Stubs for circular dependencies*)
@@ -119,16 +121,16 @@ Ltac T_rfc path ::=
   | [|- rfc _ ?b] => let b' := (eval hnf in b) in
     lazymatch b' with
     (* Right focus on positive atom, must solve the branch*)
-    | Atom Pos _ => solve [apply rfc_I_r ; [> T_exh | T_has_entry | apply Pos_atom | apply Is_atom]]
+    | Atom Pos _ => apply rfc_I_r ; [> T_exh | T_has_entry | apply Pos_atom | apply Is_atom]
 
     (* Right focus on True, must solve the branch*)
-    | True => solve [apply rfc_R_True ; T_exh]
+    | True => apply rfc_R_True ; T_exh
 
     (* Right focus on positive conjunction*)
     | AndP _ _ => apply rfc_R_AndP ; [> T_exh | T_rfc path | T_rfc path]
 
     (* Right focus on disjunction. Try proving B1, if it fails, try proving B2*)
-    | Or _ _ => solve [(apply rfc_R_Or_1 ; [> T_exh | T_rfc path])] || (apply rfc_R_Or_2 ; [> T_exh | T_rfc path])
+    | Or _ _ => (apply rfc_R_Or_1 ; [> T_exh | T_rfc path]) || (apply rfc_R_Or_2 ; [> T_exh | T_rfc path])
 
     (* Right focus on negative formula, ends right-focus phase, leaves a ufc subgoal*)
     | _ => apply rfc_R_r ; [> T_exh | T_negative | T_ufc_bracket path]
@@ -143,10 +145,10 @@ Ltac  T_lfc path ::=
   | [|- lfc _ ?b _] => let b' := (eval hnf in b) in
     lazymatch b' with
     (* Left focus on negative atom, must solve the branch*)
-    | Atom Neg _ => solve [apply lfc_I_l ; [>  T_exh | apply Neg_atom | apply Is_atom ]]
+    | Atom Neg _ => apply lfc_I_l ; [>  T_exh | apply Neg_atom | apply Is_atom ]
 
     (* Left focus on negative conjunction. Try proving B1, if it fails, try proving B2*)
-    | AndN _ _ => solve[(apply lfc_L_AndN_1 ; [>  T_exh | T_lfc path ])] || (apply lfc_L_AndN_2 ; [> T_exh | T_lfc path ])
+    | AndN _ _ => (apply lfc_L_AndN_1 ; [>  T_exh | T_lfc path ]) || (apply lfc_L_AndN_2 ; [> T_exh | T_lfc path ])
 
     (* Left focus on implication*)
     | Impl _ _ => apply lfc_L_Impl ; [> T_exh | T_rfc path | T_lfc path]
@@ -190,7 +192,7 @@ Ltac T_ufc_empty_private context path :=
     | True => eapply ufc_b_L_True ; [> T_has_entry | T_upd_rel_ex | T_ufc_empty_private rest path]
 
     (*Next linear assumption is False, must solve the branch*)
-    | False => solve [eapply ufc_b_L_False ; T_has_entry]
+    | False => eapply ufc_b_L_False ; T_has_entry
 
     (*Next linear assumption is a positive conjunction, we add the new assumptions to the list of linear assumption to process*)
     | AndP ?B1 ?B2 => eapply ufc_b_L_AndP ; [> T_has_entry | T_upd_rel_ex | T_ufc_empty_private ((B1, one) :: (B2, one) :: rest) path]
@@ -224,7 +226,7 @@ Ltac T_ufc_decide_right path ::=
     let k' := (eval hnf in k) in
     (*If k is positive, we right focus*)
     (*If k is negative, or right focusing fails, we left focus*)
-    solve [T_is_positive k' ; apply ufc_b_R_f ; [> T_exh | T_positive | T_rfc path]] || T_ufc_decide_left path
+    (T_is_positive k' ; apply ufc_b_R_f ; [> T_exh | T_positive | T_rfc path]) || T_ufc_decide_left path
   end
 .
 
@@ -244,7 +246,7 @@ Ltac T_ufc_decide_left_private context path :=
     (*If b is positive, or left focusing on b fail, we go to next entry*)
     | (?b, omega) :: ?rest => let b' := (eval hnf in b) in
     let npath := constr:((context', b) :: path) in
-    solve [T_is_negative b' ; T_noloop context' b path; eapply (@ufc_b_L_f _ b' _) ; [> T_exh | T_has_entry | T_negative | T_lfc npath]] || T_ufc_decide_left_private rest path
+    (T_is_negative b' ; T_noloop context' b path; eapply (@ufc_b_L_f _ b' _) ; [> T_exh | T_has_entry | T_negative | T_lfc npath]) || T_ufc_decide_left_private rest path
 
     (*On removed entry, we go to next entry*)
     | (_, zero) :: ?rest => T_ufc_decide_left_private rest path
