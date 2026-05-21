7171ce0995308b80581771b5fd099b2627ab5d94
MVisentin1 <matteo.visentin@mail.mcgill.ca>
2026-05-20 23:48:08 -0400

    refractor : switch equations to fixpoints

--- src/proofsearch/Subformula.v
+++ src/proofsearch/Subformula.v
@@ -1,5 +1,4 @@
 From Stdlib Require Import List.
-From Equations Require Import Equations.
 
 From LJF Require Import SharedLogic SharedLogic_tactics.
 
@@ -15,8 +14,8 @@ Inductive subformula : o -> o -> Prop :=
 | Sub_ImpR : forall A B C, subformula A C -> subformula A (Imp B C)
 .
 
-Lemma subformula_trans : 
-    forall {A B C : o}, 
+Lemma subformula_trans :
+    forall {A B C : o},
         subformula A B -> subformula B C -> subformula A C.
 Proof.
     intros A B C H0 H1. induction H1.
@@ -31,75 +30,64 @@ Proof.
     - apply Sub_ImpR. apply IHsubformula. apply H0.
 Qed.
 
-Equations subformulas (A : o) : list o :=
-  subformulas (Atom p n) := Atom p n :: nil;
-  subformulas TT := TT :: nil;
-  subformulas FF := FF :: nil;
-  subformulas (AndP A B) := (AndP A B) :: subformulas A ++ subformulas B;
-  subformulas (AndN A B) := (AndN A B) :: subformulas A ++ subformulas B;
-  subformulas (Or A B) := (Or A B) :: subformulas A ++ subformulas B;
-  subformulas (Imp A B) := (Imp A B) :: subformulas A ++ subformulas B
+Fixpoint subformulas (A : o) : list o :=
+    match A with
+    | Atom p n => Atom p n :: nil
+    | TT => TT :: nil
+    | FF => FF :: nil
+    | AndP A B => (AndP A B) :: subformulas A ++ subformulas B
+    | AndN A B => (AndN A B) :: subformulas A ++ subformulas B
+    | Or A B => (Or A B) :: subformulas A ++ subformulas B
+    | Imp A B => (Imp A B) :: subformulas A ++ subformulas B
+    end
 .
 
-Lemma subformulas_iff : 
+Lemma subformulas_iff :
     forall (A B: o), In A (subformulas B) <-> subformula A B.
 Proof.
     intros. split.
-    - intros. funelim (subformulas B).
-        + symmetry in Heqcall. rewrite Heqcall in H. 
-            destruct H. 
-            subst. apply Sub_Refl.
-            inversion H.
-        + symmetry in Heqcall. rewrite Heqcall in H.
-            destruct H. 
-            subst. apply Sub_Refl.
-            inversion H.
-        + symmetry in Heqcall. rewrite Heqcall in H.
-            destruct H. 
-            subst. apply Sub_Refl.
-            inversion H.
-        + symmetry in Heqcall. rewrite Heqcall in H1.
-            destruct H1. subst. apply Sub_Refl.
-            apply in_app_or in H1. inversion H1.
-            apply Sub_AndPL. apply H. apply H2.
-            apply Sub_AndPR. apply H0. apply H2.
-        + symmetry in Heqcall. rewrite Heqcall in H1.
-            destruct H1. subst. apply Sub_Refl.
-            apply in_app_or in H1. inversion H1.
-            apply Sub_AndNL. apply H. apply H2.
-            apply Sub_AndNR. apply H0. apply H2.
-        + symmetry in Heqcall. rewrite Heqcall in H1.
-            destruct H1. subst. apply Sub_Refl.
-            apply in_app_or in H1. inversion H1.
-            apply Sub_OrL. apply H. apply H2.
-            apply Sub_OrR. apply H0. apply H2.
-        + symmetry in Heqcall. rewrite Heqcall in H1.
-            destruct H1. subst. apply Sub_Refl.
-            apply in_app_or in H1. inversion H1.
-            apply Sub_ImpL. apply H. apply H2.
-            apply Sub_ImpR. apply H0. apply H2.
-    - intros. funelim (subformulas B).
-        + inversion H. apply in_eq.
-        + inversion H. apply in_eq.
-        + inversion H. apply in_eq.
-        + inversion H1 ; subst.
+    - intros. induction B ; simpl in H.
+        + destruct H. subst. apply Sub_Refl. inversion H.
+        + destruct H. subst. apply Sub_Refl. inversion H.
+        + destruct H. subst. apply Sub_Refl. inversion H.
+        + destruct H. subst. apply Sub_Refl.
+            apply in_app_or in H. inversion H.
+            apply Sub_AndPL. apply IHB1. apply H0.
+            apply Sub_AndPR. apply IHB2. apply H0.
+        + destruct H. subst. apply Sub_Refl.
+            apply in_app_or in H. inversion H.
+            apply Sub_AndNL. apply IHB1. apply H0.
+            apply Sub_AndNR. apply IHB2. apply H0.
+        + destruct H. subst. apply Sub_Refl.
+            apply in_app_or in H. inversion H.
+            apply Sub_OrL. apply IHB1. apply H0.
+            apply Sub_OrR. apply IHB2. apply H0.
+        + destruct H. subst. apply Sub_Refl.
+            apply in_app_or in H. inversion H.
+            apply Sub_ImpL. apply IHB1. apply H0.
+            apply Sub_ImpR. apply IHB2. apply H0.
+    - intros. induction B ; simpl.
+        + inversion H. left. reflexivity.
+        + inversion H. left. reflexivity.
+        + inversion H. left. reflexivity.
+        + inversion H ; subst.
             apply in_eq.
-            apply in_cons. apply in_or_app. left. apply H. apply H4.
-            apply in_cons. apply in_or_app. right. apply H0. apply H4.
-        + inversion H1 ; subst.
+            apply in_cons. apply in_or_app. left. apply IHB1. apply H2.
+            apply in_cons. apply in_or_app. right. apply IHB2. apply H2.
+        + inversion H ; subst.
             apply in_eq.
-            apply in_cons. apply in_or_app. left. apply H. apply H4.
-            apply in_cons. apply in_or_app. right. apply H0. apply H4.
-        + inversion H1 ; subst.
+            apply in_cons. apply in_or_app. left. apply IHB1. apply H2.
+            apply in_cons. apply in_or_app. right. apply IHB2. apply H2.
+        + inversion H ; subst.
             apply in_eq.
-            apply in_cons. apply in_or_app. left. apply H. apply H4.
-            apply in_cons. apply in_or_app. right. apply H0. apply H4.
-        + inversion H1 ; subst.
+            apply in_cons. apply in_or_app. left. apply IHB1. apply H2.
+            apply in_cons. apply in_or_app. right. apply IHB2. apply H2.
+        + inversion H ; subst.
             apply in_eq.
-            apply in_cons. apply in_or_app. left. apply H. apply H4.
-            apply in_cons. apply in_or_app. right. apply H0. apply H4.
+            apply in_cons. apply in_or_app. left. apply IHB1. apply H2.
+            apply in_cons. apply in_or_app. right. apply IHB2. apply H2.
 Qed.
-        
+
 Definition positive_b (A: o) : bool :=
     match A with
     | Atom Pos _ => true
@@ -142,8 +130,8 @@ Qed.
 
 Definition permeable_b (A : o) : bool :=
     match A with
-    | Atom _ _ => true   
-    | AndN _ _   => true 
+    | Atom _ _ => true
+    | AndN _ _   => true
     | Imp _ _   => true
     | _          => false
     end
@@ -158,7 +146,7 @@ Proof.
         try (inversion H; inversion H0; inversion H0).
 Qed.
 
-Definition subformulas_positive (A: o) : list o := 
+Definition subformulas_positive (A: o) : list o :=
     filter positive_b (subformulas A)
 .
 
@@ -172,7 +160,7 @@ Proof.
 Qed.
 
 
-Definition subformulas_negative (A: o) : list o := 
+Definition subformulas_negative (A: o) : list o :=
     filter negative_b (subformulas A)
 .
 
@@ -186,7 +174,7 @@ Proof.
 Qed.
 
 
-Definition subformulas_permeable (A: o) : list o := 
+Definition subformulas_permeable (A: o) : list o :=
     filter permeable_b (subformulas A)
 .
 
@@ -198,6 +186,3 @@ Proof.
     destruct H. split. apply permeable_b_iff in H0. apply H0.
     apply subformulas_iff. apply H.
 Qed.
-
-
-
