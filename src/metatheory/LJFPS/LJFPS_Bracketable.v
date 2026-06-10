10f026222cfb66c2a384861fbecb45f72505804d
MVisentin1 <matteo.visentin@mail.mcgill.ca>
2026-06-10 17:18:57 -0400

    feat : added specialized version of LJFPS_bracketable_goal

--- src/metatheory/LJFPS/LJFPS_Bracketable.v
+++ src/metatheory/LJFPS/LJFPS_Bracketable.v
@@ -11,3 +11,13 @@ Proof.
   apply Bracketable_pos. apply p.
   apply Bracketable_neg_atom. apply a. apply n.
 Qed.
+
+Lemma LJFPS_bracketable_goal_ept :
+  forall {C: pndctx} {L: octx} {K: o}, ept C L K -> bracketable K.
+Proof.
+  destruct LJFPS_bracketable_goal. apply H. Qed.
+
+Lemma LJFPS_bracketable_goal_lfc :
+  forall {C: pndctx} {N K: o}, lfc C N K -> bracketable K.
+Proof.
+  destruct LJFPS_bracketable_goal. apply H0. Qed.
